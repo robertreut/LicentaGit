38922f446a382dff048b299d183c75d743852bde
Robert Reut <reutrobert96@yahoo.com>
2018-05-16 13:37:57 +0300

    Added check for google maps availability.

--- CarPark/app/src/main/java/com/example/robert/carpark/SearchActivity.java
+++ CarPark/app/src/main/java/com/example/robert/carpark/SearchActivity.java
@@ -1,7 +1,12 @@
 package com.example.robert.carpark;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -9,5 +14,29 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        if (googleServicesAvailable()) {
+            Toast.makeText(this, "Welcome!", Toast.LENGTH_LONG).show();
+//            setContentView(R.layout.activity_main);
+//            initMap();
+//            localStorage = new DatabaseHelper(this);
+//            setMarkers();
+        } else {
+            //No google maps layout
+        }
+    }
+
+    public boolean googleServicesAvailable() {
+        GoogleApiAvailability api = GoogleApiAvailability.getInstance();
+        int isAvailable = api.isGooglePlayServicesAvailable(this);
+        if (isAvailable == ConnectionResult.SUCCESS) {
+            return true;
+        } else if (api.isUserResolvableError(isAvailable)) {
+            Dialog dialog = api.getErrorDialog(this, isAvailable, 0);
+            dialog.show();
+        } else {
+            Toast.makeText(this, "Can't Connect to play services", Toast.LENGTH_LONG).show();
+        }
+        return false;
     }
 }

8296ed035122e6ad045ed267aa1c6e711595ba15
Robert Reut <reutrobert96@yahoo.com>
2018-05-16 14:05:37 +0300

    Added search layout.

--- CarPark/app/src/main/java/com/example/robert/carpark/SearchActivity.java
+++ CarPark/app/src/main/java/com/example/robert/carpark/SearchActivity.java
@@ -7,23 +7,30 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
-public class SearchActivity extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+    GoogleMap mGoogleMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
-
         if (googleServicesAvailable()) {
             Toast.makeText(this, "Welcome!", Toast.LENGTH_LONG).show();
-//            setContentView(R.layout.activity_main);
-//            initMap();
+            setContentView(R.layout.activity_search);
+            initMap();
 //            localStorage = new DatabaseHelper(this);
 //            setMarkers();
         } else {
             //No google maps layout
         }
+    }
+
+    private void initMap() {
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapFragment);
+        mapFragment.getMapAsync(this);
     }
 
     public boolean googleServicesAvailable() {
@@ -38,5 +45,29 @@ public class SearchActivity extends AppCompatActivity {
             Toast.makeText(this, "Can't Connect to play services", Toast.LENGTH_LONG).show();
         }
         return false;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mGoogleMap = googleMap;
+//        //goToLocationZoom(46.769533, 23.590450,17);
+//
+//        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            return;
+//        }
+//        mGoogleMap.setMyLocationEnabled(true);
+////        mGoogleApiClient = new GoogleApiClient.Builder(this)
+////                .addApi(LocationServices.API)
+////                .addConnectionCallbacks(this)
+////                .addOnConnectionFailedListener(this)
+////                .build();
+////        mGoogleApiClient.connect();
     }
 }
